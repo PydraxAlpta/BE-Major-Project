581a61eec5d1bd77b03e450dab1df526dfcd5691
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-04-03 18:59:43 +0530

    test collidor script, does not work

---
+++ Assets/EthanCollisionBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanCollisionBehaviour : MonoBehaviour
+{
+    void onTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
