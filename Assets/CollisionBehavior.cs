414ba80899d5a59d5dd2e3ed27a98fae1492709a
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-04-06 15:58:40 +0530

    temp collision detector script

---
+++ Assets/CollisionBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionBehavior : MonoBehaviour
+{
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("On Collision Enter: " + collision.collider.name);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("On Collision Stay: " + collision.collider.name);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("On Collision Exit: " + collision.collider.name);
+    }
+}
