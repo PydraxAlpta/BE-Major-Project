bacb6ba26ca60c02ee2b62096ad6099e86a46af2
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-04-13 12:57:28 +0530

    made crowd controller basic structure

---
+++ Assets/Scripts/CrowdController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string geneticCode;
+    public float[] weights;
+    public bool runningUnderGA = false;
+    public List<GameObject> Pedestrians;
+    public int fitness = 100;
+    public GameObject simulation;
+    void Start()
+    {
+        // InterpretGeneticCode();
+        var currentSimulation = Instantiate(simulation);
+        currentSimulation.transform.SetParent(gameObject.transform);
+    }
+
+    private void InterpretGeneticCode()
+    {
+        throw new NotImplementedException();
+    }
+    public void UpdateFitness(int change)
+    {
+        fitness+=change;
+    }
+    public void Simulate()
+    {
+        throw new NotImplementedException();
+    }
+}
