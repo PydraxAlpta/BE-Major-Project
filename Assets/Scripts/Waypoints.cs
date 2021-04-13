36c4fd7b17944557acb0a29ea5c75820e72542a9
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-04-13 12:56:38 +0530

    script to get first waypoint

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Waypoint firstWaypoint;
+    public static Waypoint GetFirstWaypoint()
+    {
+        if (firstWaypoint != null)
+        {
+            return firstWaypoint;
+        }
+        var waypoint = GameObject.FindObjectOfType<Waypoint>();
+        while (waypoint.previousWaypoint!=null)
+        {
+            waypoint = waypoint.previousWaypoint;
+        }
+        return firstWaypoint = waypoint;
+    }
+}
